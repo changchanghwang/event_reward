4bd829d66a902b37b5efeed4bd1de064a1119a2b
changchanghwang <hch950627@naver.com>
2025-05-20 08:49:46 +0900

    test: reward-request model test

--- apps/event-server/test/services/reward-requests/domain/model.test.ts
+++ apps/event-server/test/services/reward-requests/domain/model.test.ts
@@ -38,4 +38,41 @@ describe('RewardRequest test', () => {
       },
     );
   });
+
+  describe('Reject test', () => {
+    test('RewardRequest의 상태를 REJECTED로 변경한다', () => {
+      const rewardRequest = rewardRequestOf({
+        status: RewardRequestStatus.REQUESTED,
+      });
+
+      rewardRequest.reject();
+
+      expect(rewardRequest.status).toBe(RewardRequestStatus.REJECTED);
+    });
+
+    test.each(
+      Object.values(RewardRequestStatus).filter(
+        (status) => status !== RewardRequestStatus.REQUESTED,
+      ),
+    )(
+      'RewardRequest가 REQUESTED 상태가 아니면 예외를 던진다 - status: %s',
+      (status) => {
+        const rewardRequest = rewardRequestOf({
+          status,
+        });
+
+        expect.assertions(2);
+        try {
+          rewardRequest.reject();
+        } catch (e) {
+          expect(e.message).toBe(
+            'Can not reject Reward request(test-id). It is not requested',
+          );
+          expect(e.getResponse().errorMessage).toBe(
+            '이미 완료된 리워드 요청을 거절할 수 없습니다.',
+          );
+        }
+      },
+    );
+  });
 });
